b688469bf5f069dc64354ba8fb257156dd6a6173
um-ma <umgo0510@gmail.com>
2021-02-18 08:56:16 +0000

    Routes

--- src/Config/Routes.tsx
+++ src/Config/Routes.tsx
@@ -1,4 +1,7 @@
+import Calendar from '../Components/Calendar'
 import Home from '../Components/Home'
+import Result from '../Components/Result'
+import Search from '../Components/Search'
 
 const routes = [
   {
@@ -7,7 +10,24 @@ const routes = [
     component: Home,
     name: Home,
   },
-  
+  {
+    path: '/',
+    exact: true,
+    component: Search,
+    name: Search,
+  },
+  {
+    path: '/',
+    exact: true,
+    component: Calendar,
+    name: Calendar,
+  },
+  {
+    path: '/',
+    exact: true,
+    component: Result,
+    name: Result,
+  }
 ]
 
 export default routes
