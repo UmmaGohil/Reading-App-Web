c92404878e483b406211fdc1bb941a4642fc4633
ummagohil <umgo0510@gmail.com>
2023-02-25 20:00:33 +0000

    current changes

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Page from "../Pages/Page";
 import Search from "../Components/Search";
 import Calendar from "../Components/Calendar";
@@ -7,65 +7,67 @@ import Data from "../Data/Data.json";
 import CSS from "csstype";
 
 const Home = () => {
-  const [Result, setResult] = useState<{}[]>([]);
-  const [startDate, setStartDate] = useState<any>();
-  const [endDate, setEndDate] = useState<any>();
+	const [Result, setResult] = useState<{}[]>([]);
+	const [startDate, setStartDate] = useState<any | number>();
+	const [endDate, setEndDate] = useState<any | number>();
 
-  const searchChange = (e: any) => {
-    const searchResult = Data.data.filter((element) =>
-      element.title.includes(e.target.value)
-    );
-    setResult(searchResult);
-  };
+	const searchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const searchResult = Data.data.filter((element) =>
+			element.title.includes(e.target.value),
+		);
+		setResult(searchResult);
+	};
 
-  const row: CSS.Properties = {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    width: "100%",
-  };
+	const row: CSS.Properties = {
+		display: "flex",
+		flexDirection: "row",
+		flexWrap: "wrap",
+		width: "100%",
+	};
 
-  const column: CSS.Properties = {
-    display: "flex",
-    flexDirection: "column",
-    flexBasis: "100%",
-    flex: "1",
-  };
+	const column: CSS.Properties = {
+		display: "flex",
+		flexDirection: "column",
+		flexBasis: "100%",
+		flex: "1",
+	};
 
-  const average: CSS.Properties = {
-    color: "#2938fa",
-    fontSize: "2rem",
-  };
+	const average: CSS.Properties = {
+		color: "#2938fa",
+		fontSize: "2rem",
+	};
 
-  const noDays = Math.ceil(
-    Math.abs(endDate - startDate) / (1000 * 60 * 60 * 24)
-  );
-  const newResult = Result;
-  const pages = newResult.map((a) => Object.values(a)[3]);
-  const parsePage: any = [...pages];
+	const noDays: number = Math.ceil(
+		Math.abs(endDate - startDate) / (1000 * 60 * 60 * 24),
+	);
+	// to do [fix var types for pags and parsePage]
+	const pages = Result.map((a: Object) => Object.values(a)[3]);
+	console.log(pages);
+	const parsePage: any = [...pages];
 
-  return (
-    <Page variant="regular">
-      <Search onChange={searchChange} />
-      <div style={row}>
-        <div style={column}>
-          <Calendar
-            startDate={startDate}
-            setStartDate={setStartDate}
-            endDate={endDate}
-            setEndDate={setEndDate}
-          />
-        </div>
-        <div style={column}>
-          <p id="my-test-id" style={average}>
-            {parsePage / noDays} pages to read each day!
-          </p>
-        </div>
-      </div>
+	return (
+		<Page variant="regular">
+			<Search onChange={searchChange} />
+			<div style={row}>
+				<div style={column}>
+					<Calendar
+						startDate={startDate}
+						setStartDate={setStartDate}
+						endDate={endDate}
+						setEndDate={setEndDate}
+					/>
+				</div>
+				<div style={column}>
+					<p id="my-test-id" style={average}>
+						{/* display message if no book input */}
+						{Math.round(parsePage / noDays)} pages to read each day!
+					</p>
+				</div>
+			</div>
 
-      <ResultData data={Result !== null ? Result : Data.data} />
-    </Page>
-  );
+			<ResultData data={Result !== null ? Result : Data.data} />
+		</Page>
+	);
 };
 
 export default Home;
--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -3,30 +3,30 @@ import Page from "../Pages/Page";
 import CSS from "csstype";
 
 interface Props {
-  onChange(e: any): void;
+	onChange(e: React.ChangeEvent<HTMLInputElement>): void;
 }
 
 const input: CSS.Properties = {
-  width: "100%",
-  height: "5rem",
-  color: "#2938fa",
-  backgroundColor: "#f1a5d0",
-  border: "none",
-  fontSize: "2rem",
-  margin: "0",
-  textAlign: "center",
+	width: "100%",
+	height: "5rem",
+	color: "#2938fa",
+	backgroundColor: "#f1a5d0",
+	border: "none",
+	fontSize: "2rem",
+	margin: "0",
+	textAlign: "center",
 };
 
 const Search = ({ onChange }: Props) => (
-  <Page variant="regular">
-    <input
-      type="search"
-      placeholder="SEARCH"
-      className="search"
-      onChange={onChange}
-      style={input}
-    />
-  </Page>
+	<Page variant="regular">
+		<input
+			type="search"
+			placeholder="SEARCH"
+			className="search"
+			onChange={onChange}
+			style={input}
+		/>
+	</Page>
 );
 
 export default Search;
